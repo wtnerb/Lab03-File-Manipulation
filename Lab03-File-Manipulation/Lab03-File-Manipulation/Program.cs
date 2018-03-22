641534d0f857ca8726d8c91a7ccd094c21bb5931
wtnerb <wtnerb@gmail.com>
2018-03-22 09:30:49 -0700

    Scaffolding

---
+++ Lab03-File-Manipulation/Lab03-File-Manipulation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab03_File_Manipulation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Lab03-File-Manipulation/UnitTestsForFileManipulation/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using static Lab03_File_Manipulation.Program;
+
+namespace UnitTestsForFileManipulation
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
