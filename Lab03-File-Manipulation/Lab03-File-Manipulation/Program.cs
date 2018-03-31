af64987e5db48fae919444963e83d60f890759d7
wtnerb <wtnerb@gmail.com>
2018-03-30 19:05:31 -0700

    added looping functionality

--- Lab03-File-Manipulation/Lab03-File-Manipulation/Program.cs
+++ Lab03-File-Manipulation/Lab03-File-Manipulation/Program.cs
@@ -24,6 +24,75 @@ namespace Lab03_File_Manipulation
             {
                 throw;
             }
+            GameLoop();
+        }
+
+        static void GameLoop()
+        {
+            Boolean playing = true;
+            Random rand = new Random();
+            string[] wordList = WordList(MY_PATH);
+            while (playing)
+            {
+                Console.Clear();
+                Console.WriteLine("Would you like to play a game?");
+                Console.WriteLine("1. Play with random word\n" +
+                                  "2. Add a word to list\n" +
+                                  "3. Remove a word from list\n" +
+                                  "4. Display word list\n" +
+                                  "5+ quit.");
+                byte option = 0;
+                for (byte count = 0; count < 10 && option < 1; count++)
+                {
+                    try
+                    {
+                        option = byte.Parse(Console.ReadLine());
+                        if (option < 1)
+                        {
+                            throw new Exception("Run git blame");
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine("invalid input");
+                    }
+                }
+                switch (option) // making sure this works in c# like in other languages. It does.
+                {
+                    case 1:
+                        //GuessingLoop(wordList[rand.Next(0, wordList.Length)]);
+                        break;
+                    case 2:
+                        Console.Clear();
+                        Console.WriteLine("What word would you like to add?");
+                        string w = Console.ReadLine().ToLower();
+                        if (Array.Exists(wordList, x=> x == w))
+                        {
+                            Console.WriteLine("That word already exists in the list!\n" +
+                                "Press any key to continue");
+                            Console.ReadKey();
+                        }
+                        AddWord(w, MY_PATH, wordList);
+                        break;
+                    case 3:
+                        //RemoveWord();
+                        break;
+                    case 4:
+                        Console.Clear();
+                        foreach(string wrd in wordList)
+                        {
+                            Console.WriteLine(wrd);
+                        }
+                        Console.WriteLine("Press any key to continue");
+                        Console.ReadKey();
+                        break;
+                    case 5:
+                        playing = false;
+                        break;
+                    default: throw new Exception("this should never run");
+                }
+            }
+
         }
 
         public static bool MatchExists(string match, string source)
@@ -50,13 +119,17 @@ namespace Lab03_File_Manipulation
             return whiteSpace.Replace(text, ",").Split(',');
         }
 
-        public static void AddWord (string word, string path)
+        public static void AddWord (string word, string path, string[] wl)
         {
             try
             {
                 using (StreamWriter sw = new StreamWriter(path))
                 {
                     sw.WriteLine(word);
+                    foreach(string w in wl)
+                    {
+                        sw.WriteLine(w);
+                    }
                 }
             }
             catch
